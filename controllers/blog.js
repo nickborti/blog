8d0ec890b8e7b7a64e115c7378dff81482677b1d
nickborti <saaggy18@gmail.com>
2022-05-26 18:25:14 +0530

    pushing tags and categories

--- controllers/blog.js
+++ controllers/blog.js
@@ -9,6 +9,11 @@ const {stripHtml} = require('string-strip-html')
 const _ = require('lodash')
 
 const {errorHandler} = require('../helpers/dbErrorHandler')
+const {smartTrim} = require('../helpers/blog')
+
+// const pushToBlog = () => {
+
+// }
 
 
 exports.create = (req, res) => {
@@ -24,13 +29,41 @@ exports.create = (req, res) => {
 
         const {title, body, categories, tags} = fields // extract these
 
+        if(!title || !title.length) {
+            return res.status(400).json({
+                error: 'Title is required'
+            })
+        }
+
+        if(!body || body.length < 200) {
+            return res.status(400).json({
+                error: 'Content is too short'
+            })
+        }
+
+        if(!categories || categories.length === 0) {
+            return res.status(400).json({
+                error: 'Atleast one category is required'
+            })
+        }
+
+        if(!tags || tags.length === 0) {
+            return res.status(400).json({
+                error: 'Atleast one tag is required'
+            })
+        }
+        
         let blog = new Blog()
         blog.title = title
         blog.body = body
+        blog.excerpt = smartTrim(body, 320, ' ', ' ...')
         blog.slug = slugify(title).toLowerCase()
         blog.mtitle = `${title} | ${process.env.APP_NAME}`
         blog.mdesc = stripHtml(body.substring(0, 160)).result // first 160 characters
         blog.postedBy = req.profile._id
+
+        let arrayOfCategories = categories && categories.split(',')
+        let arrayOfTags = tags && tags.split(',')
        
         if(files.photo) {
             if(files.photo.size > 10000000) { // > 1MB in bytes
@@ -50,7 +83,25 @@ exports.create = (req, res) => {
             })
            }
            
-           res.json(result)
+        //    res.json(result)
+        Blog.findByIdAndUpdate(result._id, {$push: {categories: arrayOfCategories}}, {new: true}).exec((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            } else {
+                Blog.findByIdAndUpdate(result._id, {$push: {tags: arrayOfTags}}, {new: true}).exec((err, result) => {
+                    if(err) {
+                        return res.status(400).json({
+                            error: errorHandler(err)
+                        })
+                    }
+
+                    res.json(result)
+                    
+                })
+            }
+        })
        })
     })
 }
